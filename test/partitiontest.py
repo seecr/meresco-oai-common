610f6d95d1847eef13343e1f4584969914369d80
Thijs Janssen <thijs@seecr.nl>
2018-03-07 11:43:13 +0100

    TJ: license.

--- test/partitiontest.py
+++ test/partitiontest.py
@@ -1,25 +1,24 @@
 ## begin license ##
 #
-# "Meresco Oai" are components to build Oai repositories, based on
-# "Meresco Core" and "Meresco Components".
+# "Meresco Oai Utils" are utils to support "Meresco Oai".
 #
 # Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
-# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2015, 2018 Seecr (Seek You Too B.V.) http://seecr.nl
 #
-# This file is part of "Meresco Oai"
+# This file is part of "Meresco Oai Utils"
 #
-# "Meresco Oai" is free software; you can redistribute it and/or modify
+# "Meresco Oai Utils" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Meresco Oai" is distributed in the hope that it will be useful,
+# "Meresco Oai Utils" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Oai"; if not, write to the Free Software
+# along with "Meresco Oai Utils"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
